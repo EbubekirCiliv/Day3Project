26098d1a3306bfc0d6c270e5a66f766fcf1d490d
EbubekirCiliv <72078134+EbubekirCiliv@users.noreply.github.com>
2020-10-06 21:59:18 +0200

    added awesome stuff

--- src/test/java/AwesomeTest.java
+++ src/test/java/AwesomeTest.java
@@ -4,4 +4,8 @@ public class AwesomeTest {
     // green means its under staging area
     //grey in idea folder means is ignored
 
+    // I JUST PULLED NEW CAHNGE FROM GITHUB I DID NOT HAVE LOCALLY AND NOW MY PROJECT IS IN SYNC WITH REMOTE REPOSITORY
+    // and now i made awesome changes and i m about to push this back to github
+
+
 }
